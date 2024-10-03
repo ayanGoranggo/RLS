4041489a5238e38749108d0f0610e3a9aa027d21
ayanGoranggo <ayan.munshi@goranggosolutions.com>
2024-10-03 15:22:21 +0530

    .env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://xsllesqxcnognegmvtfx.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzbGxlc3F4Y25vZ25lZ212dGZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3ODgzNTEsImV4cCI6MjA0MzM2NDM1MX0.RN6coy76bwRFJTYTrcgtRtKdd3ZoDuy0ZpjLVzWyIRQ";
+const supabaseKey = import.meta.env.VITE_SUPABASE_TOKEN;
 export const supabase = createClient(supabaseUrl, supabaseKey);
